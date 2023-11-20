4668627e9814c1357221c758e781d3c544708598
deepakcoder1 <deepakkumarrasya@gmail.com>
2023-11-20 12:25:34 +0530

    bulb project

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+   const bulbobj = document.querySelector(".bulbofff");
+   bulbobj.setAttribute("src", "images/on.png")
+}
+function bulbOff(){
+    const bulbobj = document.querySelector(".bulbofff");
+    bulbobj.setAttribute("src", "images/bulb1.png")
+ }
